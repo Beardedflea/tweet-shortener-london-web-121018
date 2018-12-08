c270159b9799ce63be880021ff2e31a882ba21b1
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-08 14:23:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary
+  dictionary ={
+    :hello => "hi",
+    :to => "2",
+    :two => "2",
+    :too => "2",
+    :for => "4",
+    :four => "4",
+    :be => "b",
+    :you => "u",
+    :at => "@",
+    :and => "&"
+    }
+end
+
+def word_substituter(tweet)
+  cool_kids_tweet = tweet.split
+  cool_kids_tweet.each do |word|
+    if word.eql?(dictionary) == true
+      word.replace(dictionary)
+    end
+   end 
+   cool_kids_tweet
+end
